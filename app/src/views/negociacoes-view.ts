da6255eb6f3ee760894a14f8e0974037ece2d29c
GabrielArmond <gabriel.guerra@farme.com.br>
2022-08-09 13:17:58 -0300

    avançando no curso

--- app/src/views/negociacoes-view.ts
+++ app/src/views/negociacoes-view.ts
@@ -1,14 +1,11 @@
+import { escape } from "../decorators/escape.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-export class NegociacoesView {
-
-  private elemento: HTMLElement;
-
-  constructor(selector: string) {
-    this.elemento = document.querySelector(selector);
-  }
-
-  template(model: Negociacoes): string {
+export class NegociacoesView extends View<Negociacoes> {
+  
+  @escape
+  protected template(model: Negociacoes): string {
     return `
     <table class="table table-hover table-bordered">
       <thead>
@@ -23,7 +20,7 @@ export class NegociacoesView {
         ${model.lista().map(negociacao => {
           return `
             <tr>
-              <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+              <td>${this.formatar(negociacao.data)}</td>
               <td>${negociacao.quantidade}</td>
               <td>${negociacao.valor}</td>
               <td>${negociacao.volume}</td>
@@ -35,9 +32,7 @@ export class NegociacoesView {
     `;
   }
 
-  update(model: Negociacoes): void {
-    const template = this.template(model);
-    console.log(template)
-    this.elemento.innerHTML = template;
+  private formatar(data: Date): string {
+    return new Intl.DateTimeFormat().format(data)
   }
 }
